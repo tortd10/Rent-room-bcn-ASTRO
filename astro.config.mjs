b7ceeb0d7d7dfd8e3691a1f05f40620c272c7406
David <tortd10@gmail.com>
2024-12-01 21:52:51 +0100

    MultiIdioma

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+// @ts-ignore
+import i18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    i18next({
+      default: 'es', // Idioma por defecto
+      locales: ['en', 'es'], // Idiomas soportados
+    }),
+  ],
 });
