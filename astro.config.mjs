dd65ffdd04b0086d31df63f1724a8084949b784f
David <tortd10@gmail.com>
2024-12-29 05:12:49 +0100

    Google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,20 @@ import tailwind from '@astrojs/tailwind';
 // @ts-ignore
 import i18next from 'astro-i18next';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	build: {
-		inlineStylesheets: "always",
-	},
-	compressHTML: true,
-	prefetch: true,
-	devToolbar: {
-		enabled: false,
-	},
-  integrations: [
-    tailwind(),
-    i18next({
-      default: 'es', // Idioma por defecto
-      locales: ['en', 'es'], // Idiomas soportados
-    }),
-	db()
-  ],
+    build: {
+        inlineStylesheets: "always",
+    },
+    compressHTML: true,
+    prefetch: true,
+    devToolbar: {
+        enabled: false,
+    },
+  integrations: [tailwind(), i18next({
+    default: 'es', // Idioma por defecto
+    locales: ['en', 'es'], // Idiomas soportados
+  }), db(), partytown()],
 });
