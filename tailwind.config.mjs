eb206f5e180894a0746a9259f38d27a767fcf27e
David <tortd10@gmail.com>
2025-03-06 17:20:21 +0100

    Refactor Green

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
-				primary: '#1E1B4B',
-				secondary: '#4F46E5',
-				texth2: '#3730A3',
-				primaryBg: '#EEF2FF',
-				headerBg: '#D0D9F9'
+				primary: "#344E41",
+				secondary: "#3A5A40",
+				texth2: "black",
+				primaryBg: "#DAD7CD",
+				headerBg: "#A3B18A",
 			},
-			
 		},
 	},
 	plugins: [],
