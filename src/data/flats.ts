ca73da97c9b54689d55c52fa10812f046f1e9c8c
David <tortd10@gmail.com>
2025-03-08 15:16:00 +0100

    Fixed PLP Rooms

--- src/data/flats.ts
+++ src/data/flats.ts
@@ -2,19 +2,22 @@ export const FLATS = [
 	{
 		mapImage:
 			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.654874799709!2d2.14123587646369!3d41.35985469770182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a2098d5ce117%3A0x333f0cc33b903f8e!2sCarrer%20dels%20Ferrocarrils%20Catalans%2C%20Sants-Montju%C3%AFc%2C%2008038%20Barcelona!5e0!3m2!1sca!2ses!4v1732368154846!5m2!1sca!2ses",
-		title: "Carrer dels Ferrocarrils Catalans",
+		title: "Ferrocarrils Catalans",
+		street: "Carrer dels Ferrocarrils Catalans",
 		flat_id: "FFCC71",
 	},
 	{
 		mapImage:
 			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.310902770423!2d2.129166476464017!3d41.36732949723848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4989139122e81%3A0x622c80e963584c7e!2sCarrer%20de%20la%20Riera%20Blanca%2C%20198%2C%2008902%20L&#39;Hospitalet%20de%20Llobregat%2C%20Barcelona!5e0!3m2!1sca!2ses!4v1732368642766!5m2!1sca!2sep",
-		title: "Carrer de la Riera Blanca",
+		title: "Riera Blanca",
+		street: "Carrer de la Riera Blanca",
 		flat_id: "RB219",
 	},
 	{
 		mapImage:
 			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d748.6976703683592!2d2.1350064762711103!3d41.356903200226924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a498a342294dc9%3A0xcf3402c425cbbcaf!2sCarrer%20dels%20Alts%20Forns%2C%2029X%2C%20Sants-Montju%C3%AFc%2C%2008038%20Barcelona!5e0!3m2!1sca!2ses!4v1739468852814!5m2!1sca!2ses",
-		title: "Carrer dels Alts Forns",
-		flat_id: "AAFFXXX",
+		title: "Alts Forns",
+		street: "Carrer dels Alts Forns",
+		flat_id: "AAFF33",
 	},
 ]
